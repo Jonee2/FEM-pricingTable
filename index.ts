08da1253d98642a127d8df891658a462869319d4
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-02-10 22:20:26 -0300

    project done

--- index.ts
+++ index.ts
@@ -6,9 +6,9 @@ let price = document.querySelectorAll('.liprice')
 
 toggle?.addEventListener('click', function () {
     if (toggle.checked == true) {
-        price[0].textContent = "$19.99";
-        price[1].textContent = "$24.99"
-        price[2].textContent = "$39.99"
+    price[0].textContent = "$19.99";
+    price[1].textContent = "$24.99"
+    price[2].textContent = "$39.99"
     } else {
     price[0].textContent = "$199.99";
     price[1].textContent = "$249.99"
