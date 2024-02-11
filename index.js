e0b39f740b0b2e44e3c08c738ab7f36a23493e97
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-02-10 21:57:27 -0300

    script done

---
+++ index.js
@@ -0,0 +1,17 @@
+"use strict";
+//TODO button toggle and change text
+// 19,99, 24,99, 39,99 
+let toggle = document.querySelector('.toggle');
+let price = document.querySelectorAll('.liprice');
+toggle === null || toggle === void 0 ? void 0 : toggle.addEventListener('click', function () {
+    if (toggle.checked == true) {
+        price[0].textContent = "$19.99";
+        price[1].textContent = "$24.99";
+        price[2].textContent = "$39.99";
+    }
+    else {
+        price[0].textContent = "$199.99";
+        price[1].textContent = "$249.99";
+        price[2].textContent = "$399.99";
+    }
+});
---
+++ index.ts
@@ -0,0 +1,21 @@
+//TODO button toggle and change text
+// 19,99, 24,99, 39,99 
+
+let toggle = document.querySelector('.toggle')
+let price = document.querySelectorAll('.liprice')
+
+toggle?.addEventListener('click', function () {
+    if (toggle.checked == true) {
+        price[0].textContent = "$19.99";
+        price[1].textContent = "$24.99"
+        price[2].textContent = "$39.99"
+    } else {
+    price[0].textContent = "$199.99";
+    price[1].textContent = "$249.99"
+    price[2].textContent = "$399.99"
+    }
+})
+
+
+
+
